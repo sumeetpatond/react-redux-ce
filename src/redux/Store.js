8ff8f2daf4f915dadf71a8e46ef66ee24be651fb
sumeet-07 <sumeetpatond@gmail.com>
2021-10-13 00:35:02 +0530

    applyMiddleware

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 import Reducer from "./Reducer";
 
-const store = createStore(Reducer);
+const store = createStore(Reducer, applyMiddleware(logger));
 
 export default store;
